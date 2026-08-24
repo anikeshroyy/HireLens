b7068602cc9a7a6a7c58ade3b44fcad7fe927764
Anikesh Roy <anikeshroy62040@gmail.com>
2026-08-24 08:00:38 +0530

    refactor: improve error handling and response structure in loginUser function

--- Backend/src/controllers/loginController.js
+++ Backend/src/controllers/loginController.js
@@ -9,21 +9,25 @@ const loginUser = async (req, res) => {
     try {
         const user = await userModel.findOne({ email: req.body.email })
         if (!user) {
-            console.log("email is incorrect")
-            return res.send("Either email or password incoreect")
+            console.log("Email is incorrect")
+            return res.status(401).json({
+                message: "Invalid Email or Password"
+            })
         }
 
-        await bcrypt.compare(req.body.password, user.password, (err, result) => {
-            if (!result) return res.send("password not matched")
-
-            const token = jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRETS)
-
-            res.cookie("token", token)
-            res.status(200).json({
-                message: "User LoggedIn"
-            })
+        const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
+        if (!isPasswordCorrect) return res.status(401).json({
+            message: "Invalid Email or Password"
         })
-    } catch (error) {
+
+        const token = jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRETS)
+
+        res.cookie("token", token)
+        res.status(200).json({
+            message: "User LoggedIn"
+        })
+    }
+    catch (error) {
         res.status(500).json({
             message: "something went wrong",
             error: error.message
